613622ec931fb09aa1a8d456afe7d3168df1b5a6
LuanSilva9 <luanpdev@gmail.com>
2024-09-27 14:55:24 -0400

    Testando classe principal

--- src/main/java/org/ecommerce/Main.java
+++ src/main/java/org/ecommerce/Main.java
@@ -1,9 +1,17 @@
 package org.ecommerce;
 
 import org.models.ECommerce;
+import org.models.Gestor;
+import org.models.lists.ListaGestor;
+
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
         ECommerce eCommerce = new ECommerce();
+
+        ListaGestor listaGestor = new ListaGestor();
+
+        Gestor test = new Gestor(UUID.randomUUID(), "Luan", "000.000.000-01", "luan.getjava@email.com", eCommerce);
     }
 }
